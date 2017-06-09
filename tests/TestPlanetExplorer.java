336701d66db2f7604873ea9e05c6016a2a3623ba
somename <someemail>
2017-06-09 11:59:24 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -3,9 +3,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPlanetExplorer {
-
+/*
 	@Test
 	public void test_executeCommand() {
 		fail("Not yet implemented");
+	}
+	*/
+	@Test
+	public void test_move_forth_north(){
+		PlanetExplorer pe = new PlanetExplorer(3,3,null);
+		
+		assertEquals("", "(0,0,n", pe.executeCommand("f"));
 	}
 }
